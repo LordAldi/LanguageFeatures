ac4228ce7ebc5dd7fa7ff1dcc489a8090d8af33b
LordAldi <aldianugra09@gmail.com>
2021-01-11 19:33:15 +0700

    Chaining the Null Conditional Operator

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,6 +9,7 @@ namespace LanguageFeatures.Models
     {
         public string Name { get; set; }
         public decimal? Price { get; set; }
+        public Product Related { get; set; }
         public static Product[] GetProducts()
         {
             Product kayak = new Product
@@ -21,6 +22,7 @@ namespace LanguageFeatures.Models
                 Name = "Life Jacket",
                 Price = 48.95M
             };
+             kayak.Related = lifeJacket;
 
             return new Product[] { kayak, lifeJacket, null };
         }
