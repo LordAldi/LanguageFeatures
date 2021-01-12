005ea616e5d2391aa298c592a0b1aefdebc366e2
LordAldi <aldianugra09@gmail.com>
2021-01-12 20:42:31 +0700

    Adding a Feature in the IProductSelection.cs

--- Models/IProductSelection.cs
+++ Models/IProductSelection.cs
@@ -8,5 +8,7 @@ namespace LanguageFeatures.Models
     interface IProductSelection
     {
         IEnumerable<Product> Products { get; }
+
+        IEnumerable<string> Names => Products.Select(p => p.Name);
     }
 }
