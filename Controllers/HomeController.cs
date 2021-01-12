2ab274eff90c82c0fb8683d6b7c77a96001824c8
LordAldi <aldianugra09@gmail.com>
2021-01-12 19:21:13 +0700

    Filter with Defining Functions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using LanguageFeatures.Models;
-
+using System;
 
 namespace LanguageFeatures.Controllers
 {
@@ -26,6 +26,10 @@ namespace LanguageFeatures.Controllers
                 ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
             };
             return View("Index", products.Keys);*/
+            bool FilterByPrice(Product p)
+            {
+                return (p?.Price ?? 0) >= 20;
+            };
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
             Product[] productArray = {
                  new Product {Name = "Kayak", Price = 275M},
@@ -33,8 +37,19 @@ namespace LanguageFeatures.Controllers
                  new Product {Name = "Soccer ball", Price = 19.50M},
                  new Product {Name = "Corner flag", Price = 34.95M}
              };
-            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
-            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+
+            Func<Product, bool> nameFilter = delegate (Product prod)
+             {
+                 return prod?.Name[0] == 'S';
+             };
+
+            decimal priceFilterTotal = productArray
+                .Filter(FilterByPrice)
+                .TotalPrices();
+            decimal nameFilterTotal = productArray
+                .Filter(nameFilter)
+                .TotalPrices();
+
             return View("Index", new string[] {
                  $"Price Total: {priceFilterTotal:C2}",
                  $"Name Total: {nameFilterTotal:C2}" 
--- Models/MyExtensionMethods.cs
+++ Models/MyExtensionMethods.cs
@@ -16,26 +16,17 @@ namespace LanguageFeatures.Models
             }
             return total;
         }
-        public static IEnumerable<Product> FilterByPrice( this IEnumerable<Product> productEnum, decimal minimumPrice)
+        public static IEnumerable<Product> Filter( this IEnumerable<Product> productEnum, Func<Product, bool> selector)
         {
             foreach(Product prod in productEnum)
             {
-                if((prod?.Price ?? 0 ) >= minimumPrice && prod != null)
+                if(selector(prod))
                 {
                     yield return prod;
                 }
             }
         }
 
-        public static IEnumerable<Product> FilterByName(this IEnumerable<Product> productEnum, char firstLetter)
-        {
-            foreach (Product prod in productEnum)
-            {
-                if (prod?.Name?[0] == firstLetter && prod != null)
-                {
-                    yield return prod;
-                }
-            }
-        }
+        
     }
 }
