943004b7d36863fc0d9373ec9d67f36a41028192
LordAldi <aldianugra09@gmail.com>
2021-01-13 00:25:32 +0700

    Getting Names using nameof

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -63,16 +63,28 @@ namespace LanguageFeatures.Controllers
              );
             return View(cart.Names);
         }*/
-        public async Task<ViewResult> Index()
+        /*public async Task<ViewResult> Index()
         {
-            /*long? length = await MyAsyncMethods.GetPageLength();
-            return View(new string[] { $"Length: {length}" });*/
+            *//*long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Length: {length}" });*//*
             List<string> output = new List<string>();
             await foreach (long? len in MyAsyncMethods.GetPageLengths(output,"apress.com", "microsoft.com", "amazon.com"))
             {
                 output.Add($"Page length: { len}");
             }
             return View(output);
+        }*/
+
+        public ViewResult Index()
+        {
+            var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+
+            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
         }
 
     }
